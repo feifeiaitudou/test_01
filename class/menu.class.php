16e376b468ff5bdaf1dd186a103d2f7d8b55a327
heliang_windows <heliang_windows@example.com>
2017-06-23 16:11:21 +0800

    class file

---
+++ class/menu.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * 测试的菜单内容
+ * 
+ */
+
+
+ class Menu()
+ {
+    private $name;
+    private $age;
+
+
+    public function __construct($name, $age)
+    {
+      $this->name = $name;
+      $this->age = $age;
+    }
+ 
+ }
